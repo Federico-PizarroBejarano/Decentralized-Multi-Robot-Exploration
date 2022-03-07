90e80bc6f6d7b3a926cd34a4ed683604eb82bc70
richa.ren@mail.utoronto.ca <richa.ren@mail.utoronto.ca>
2022-03-07 16:33:35 -0500

    add path for asblab

--- decentralized_exploration/dme_drl/constants.py
+++ decentralized_exploration/dme_drl/constants.py
@@ -4,9 +4,10 @@ import shutil
 
 if 'chi-hong' in os.getcwd():
     PROJECT_PATH = '/home/chi-hong/Desktop/RICHARD/Decentralized-Multi-Robot-Exploration/decentralized_exploration/dme_drl'
+elif 'asblab' in os.getcwd():
+    PROJECT_PATH = '/home/asblab/Desktop/RICHARD/Decentralized-Multi-Robot-Exploration/decentralized_exploration/dme_drl'
 else:
     PROJECT_PATH = '/Users/richardren/VisualStudioCodeProjects/Decentralized-Multi-Robot-Exploration/decentralized_exploration/dme_drl'
-
 
 CONFIG_PATH = PROJECT_PATH + '/assets/config.yaml'
 MODEL_DIR = PROJECT_PATH + '/model/'
